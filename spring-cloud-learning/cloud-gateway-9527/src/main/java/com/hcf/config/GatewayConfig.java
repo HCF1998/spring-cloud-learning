1428ea05397fe02831be634d3958fdc2b167f9d0
Abel.changfeng.he <he_changfeng@163.com>
2023-01-03 22:48:18 +0800

    [2023-01-03] 1.通过Gateway配置类配置注入RouteLocator Bean实现路由 2.实现通过服务注册中心根据路由名称实现动态路由切换。

---
+++ spring-cloud-learning/cloud-gateway-9527/src/main/java/com/hcf/config/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.hcf.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        // 路由id
+        return routes.route("path_route_first",
+                // 路由
+                r -> r.path("/guangzhou")
+                        // 映射地址
+                        .uri("http://pc.nfapp.southcn.com/guangzhou")).build();
+
+    }
+}
