289987988ce32654b70d40f212726234de3033b4
Abel.changfeng.he <he_changfeng@163.com>
2023-01-04 22:16:05 +0800

    [2023-01-04] 1.配置路由匹配规则

---
+++ spring-cloud-learning/cloud-gateway-9527/src/main/java/com/hcf/util/TimeUtils.java
@@ -0,0 +1,10 @@
+package com.hcf.util;
+
+import java.time.ZonedDateTime;
+
+public class TimeUtils {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
